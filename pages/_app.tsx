97c2e345bbc6bcd071119c24dc5c0dfc705e99c4
al3sha9 <optimist.alishan@gmail.com>
2023-02-07 21:10:08 +0500

    Responsive NAVBAR added to home page, Small tweaks left

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,12 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 import { MeshProvider } from "@meshsdk/react";
+import Header from "./header";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <MeshProvider>
+      <Header />
       <Component {...pageProps} />
     </MeshProvider>
   );
