087b7e7ab003cd7c8527b10aab4bfd860ed1ae5a
al3sha9 <optimist.alishan@gmail.com>
2023-02-11 17:56:52 +0500

    lint clear

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Menu from "../components/menu";
-import Timeline from "../components/timeline";
+// import Timeline from "../components/timeline";
 
 const Home = () => {
   return (
