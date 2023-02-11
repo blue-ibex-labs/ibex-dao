0cbfa7c1896a3b8405c4a36c56faa66e51827b18
hunzai <amjadsaleem@gmail.com>
2023-02-11 13:50:58 +0100

    minor improvements

--- pages/minting.tsx
+++ pages/minting.tsx
@@ -49,44 +49,59 @@ export default function Minting() {
         <div className="flex h-screen items-center bg-gradient-to-l from-black via-stone-600"></div>
       </div>
       <div className="w-11/12">
-        <div className="grid place-content-start md:place-content-center">
-          <div className="flex flex-row">
-            {connected ? (
-              <div className="grid grid-rows-3 grid-flow-col gap-4">
-                <div className=" flex items-center  justify-between p-4  rounded-lg bg-white shadow-indigo-50 shadow-md">
+        <div className="flex flex-col">
+          <div className="flex flex-row items-center justify-center">
+            <div>
+              <h6 className="md:text-4xl  sm:tracking-tight text-gray-50 flex flex-row item-center">
+                Mint an Ibex NFT to join the Tribe
+              </h6>
+            </div>
+          </div>
+          <div className="flex flex-col "></div>
+          <div className="flex flex-col space-y-10 items-center justify-center">
+            <div className="grid place-content-start md:place-content-center">
+              <div>
+                {connected ? (
                   <div>
-                    <h2 className="text-gray-900 text-lg font-bold">
-                      Balance <b>₳ {parseInt(lovelace) / 1000000}</b>.
-                    </h2>
-                    <h3 className="mt-2 text-xl font-bold text-black text-left">
-                      + 150.000 ₭
-                    </h3>
-                    <p className="text-sm font-semibold text-gray-400">
-                      Last Transaction
-                    </p>
-                    <button
-                      className="text-sm mt-6 px-4 py-2 bg-yellow-400 text-white rounded-lg  tracking-wider hover:bg-yellow-300 outline-none"
-                      type="button"
-                      onClick={() => startMining()}
-                      disabled={loading}
-                    >
-                      Mint
-                    </button>
+                    <div className=" flex items-center  justify-between p-4  rounded-lg bg-white shadow-indigo-50 shadow-md">
+                      <div>
+                        <h2 className="text-gray-900 text-lg font-bold">
+                          Balance <b>₳ {parseInt(lovelace) / 1000000}</b>.
+                        </h2>
+                        <h3 className="mt-2 text-xl font-bold text-black text-left">
+                          + 150.000 ₭
+                        </h3>
+                        <p className="text-sm font-semibold text-gray-400">
+                          Last Transaction
+                        </p>
+                        <button
+                          className="text-sm mt-6 px-4 py-2 bg-indigo-400 text-white rounded-lg  tracking-wider hover:bg-yellow-300 "
+                          type="button"
+                          onClick={() => startMining()}
+                          disabled={loading}
+                        >
+                          Mint
+                        </button>
+                      </div>
+                    </div>
+                    <div className="flex flex-row justify-center items-center p-4">
+                      <div className="text-white">
+                        <p>
+                          {loading
+                            ? "Creating transaction..."
+                            : "Ready to mint"}
+                        </p>
+                      </div>
+                    </div>
                   </div>
-                </div>
-                <div className="flex flex-row justify-center items-center ">
-                  <div className="text-white">
-                    <p>
-                      {loading ? "Creating transaction..." : "Ready to mint"}
-                    </p>
-                  </div>
-                </div>
+                ) : (
+                  <CardanoWallet />
+                )}
               </div>
-            ) : (
-              <CardanoWallet />
-            )}
+            </div>
           </div>
         </div>
+
         <div className="flex flex-row">
           {txHash && (
             <div>
