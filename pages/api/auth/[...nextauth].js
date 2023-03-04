ebe437639c819f7bbd3e7fcb4272169c77b58698
al3sha9 <optimist.alishan@gmail.com>
2023-03-04 15:01:23 +0500

    added coinbase 1

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import CoinbaseProvider from "next-auth/providers/coinbase";
 import TwitterProvider from "next-auth/providers/twitter";
 import GoogleProvider from "next-auth/providers/google";
 
