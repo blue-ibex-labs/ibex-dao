84bf2c1559774d6c55850359aaf8d41a900902a3
al3sha9 <optimist.alishan@gmail.com>
2023-03-12 00:56:41 +0500

    added a little styling

--- components/login-btn.jsx
+++ components/login-btn.jsx
@@ -1,4 +1,5 @@
 import { useSession, signIn, signOut } from "next-auth/react"
+import Image from "next/image"
 
 
 export default function LOGIN() {
@@ -8,7 +9,7 @@ export default function LOGIN() {
         return (
             <>
 
-                <h2>Signed in as {session.user.name}</h2> <br />
+                <h2 className="md:text-2xl  sm:tracking-tight text-gray-50">Signed in as <b>{session.user.name}</b></h2> <br />
                 <button className="hover:shadow-form rounded-md bg-[#6A64F1] py-3 px-8 text-center text-base font-semibold text-white outline-none" onClick={() => signOut()}>Sign out</button>
             </>
         )
