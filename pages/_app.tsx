0a987173c4e2a4cb552ee9f2f4833a89e26c3884
Ali Shan <optimist.alishan@gmail.com>
2023-05-01 00:09:35 +0500

    removed some junk

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,10 @@
 import "../styles/globals.css";
-// import dynamic from 'next/dynamic'
 import type { AppProps } from "next/app";
 
 import { MeshProvider } from "@meshsdk/react";
 import Header from "../components/header";
 import Head from "next/head";
 import 'atropos/css'
-
-
-// const AnimatedCursor = dynamic(() => import('react-animated-cursor'), {
-//   ssr: false
-// });
 
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -22,16 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 
       </Head>
       <MeshProvider>
-        {/* <AnimatedCursor color="0,0,0"
-        innerSize={8}
-        outerSize={20}
-        innerScale={1}
-        outerScale={1.7}
-        outerAlpha={0}
-        trailingSpeed={4}
-        outerStyle={{
-          border: '3px solid #000'
-        }} /> */}
+
         <Header />
         <Component {...pageProps} />
 
