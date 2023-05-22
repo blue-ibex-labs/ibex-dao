02513e0be09bd0e8292cad13c56c18f11befea35
Ali Shan <optimist.alishan@gmail.com>
2023-05-22 19:36:41 +0500

    removed anim

--- tests/test.js
+++ tests/test.js
@@ -1,10 +1,20 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import MintingForm from "../pages/form";
+/* globals describe, expect , test*/
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom'
+import HeaderBG from '../components/header_bg';
 
-describe("Jest Snapshot testing suite", () => {
-  it("Matches DOM Snapshot", () => {
-    const domTree = renderer.create(<MintingForm />).toJSON();
-    expect(domTree).toMatchSnapshot();
+describe('HeaderBG component', () => {
+  test('renders header text', () => {
+    const { getByText } = render(<HeaderBG />);
+    const titleElement = getByText(/BLUE IBEX DAO./i);
+    const subtitleElement = getByText(/DEFi For Everyone!/i);
+    expect(titleElement).toBeInTheDocument();
+    expect(subtitleElement).toBeInTheDocument();
+  });
+
+  test('renders with expected styles', () => {
+    const { container } = render(<HeaderBG />);
+    expect(container.firstChild).toHaveClass('h-screen flex bg-gradient-cus');
   });
 });
